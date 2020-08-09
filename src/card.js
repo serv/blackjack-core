9a9b5b216319b741a9653fab834afdc0edc6fd5e
Jason Kim <iamjsonkim@gmail.com>
2020-08-08 22:20:31 -0700

    card#setupValue

--- src/card.js
+++ src/card.js
@@ -2,5 +2,22 @@ export default class Card {
   constructor({ suite, name }) {
     this.suite = suite;
     this.name = name;
+    this.values = [];
+    this.setupValue();
+  }
+
+  setupValue() {
+    switch (this.name) {
+      case "A":
+        this.values = [1, 11];
+        break;
+      case "J":
+      case "Q":
+      case "K":
+        this.values = [10];
+        break;
+      default:
+        this.values = [parseInt(this.name)];
+    }
   }
 }
